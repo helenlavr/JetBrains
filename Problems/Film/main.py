49fc8b4a14c62d2de9369405244182b02a344e42
helenlavr <helenlav@stud.ntnu.no>
2020-09-25 16:50:42 +0200

    §TTT done

---
+++ Problems/Film/main.py
@@ -0,0 +1,5 @@
+movie = input()
+director = input()
+release_year = input()
+
+print(f'{movie} (dir. {director}) came out in {release_year}')
---
+++ Problems/Tax brackets/main.py
@@ -0,0 +1,15 @@
+income = int(input())
+
+if income >= 132407:
+    percent = 28
+elif income >= 42708:
+    percent = 25
+elif income >= 15528:
+    percent = 15
+else:
+    percent = 0
+
+calculated_tax = round(income * percent * 0.01)
+
+
+print(f'The tax for {income} is {percent}%. That is {calculated_tax} dollars!')
